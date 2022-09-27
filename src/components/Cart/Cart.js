db772aabbf10c454f3851790c61f7e058ac5a696
sabbirahmedseam <sabbirahmedseam1@gmail.com>
2022-09-27 07:53:45 +0600

    send

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,32 @@
 import React from "react";
+import "./Cart.css";
 
 const Cart = ({ cart }) => {
+//   console.log(cart);
+  let total = 0;
+  let shipping = 0;
+  let quantity=0;
+  for (const product of cart) {
+   
+    quantity=quantity+product.quantity;
+    
+
+    total = total + product.price*product.quantity;
+     console.log(total);
+    shipping = shipping + product.shipping;
+  }
+  const tax = parseFloat((total * 0.1).toFixed(2));
+  const grandTotal =total + shipping + tax;
+    // console.log(typeof tax,typeof grandTotal);
+
   return (
-    <div>
+    <div className="cart">
       <h4>Order summary in Cart</h4>
-      <h5>Order Summary:{cart.length}</h5>
+      <p>Order Summary:{quantity}</p>
+      <p>Total price:${total} </p>
+      <p>Total Shipping:${shipping} </p>
+      <p>Tax:${tax} </p>
+      <p>Grand Total:${grandTotal.toFixed(2)} </p>
     </div>
   );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { addToDb, getStoredCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
@@ -8,16 +9,51 @@ const Shop = () => {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
+    // console.log("products load before fetch");
     fetch("products.json")
       .then((res) => res.json())
-      .then((data) => setProducts(data));
+      .then((data) => {
+        setProducts(data);
+        // console.log('products loaded');
+      });
   }, []);
-  const handleAddToCart = (product) => {
-    // console.log(product);
+
+  useEffect(() => {
+    // console.log("local storage first line", products);
+    const storedCart = getStoredCart();
+    const savedCart = [];
+    for (const id in storedCart) {
+      const addedProduct = products.find(
+        (product) => product.id === id
+      );
+      if (addedProduct) {
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        savedCart.push(addedProduct);
+        // console.log(addedProduct);
+      }
+    }
+    setCart(savedCart);
+    // console.log('local storage finished');
+  }, [products]);
+
+  const handleAddToCart = (selectedProduct) => {
+    console.log(selectedProduct);
+    let newCart = [];
+    const exists = cart.find((product) => product.id === selectedProduct.id);
+    if (!exists) {
+      selectedProduct.quantity = 1;
+      newCart = [...cart, selectedProduct];
+    } else {
+      const rest = cart.filter((product) => product.id !== selectedProduct.id);
+      exists.quantity=exists.quantity+1;
+      newCart=[...rest,exists];
+    }
+
     // support:data to vai nicher theke send korlam jemon 'product' to pathailamm
     //don't do this cart.push(product)  (copy korle valo normally eivabe array te bosano hoy kintu react e copy kore bosate hobe)
-    const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(selectedProduct.id);
   };
   return (
     <div className="shop_container">
